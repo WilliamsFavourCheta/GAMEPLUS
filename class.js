76f4c9c19729da5a41225a73adbad4482c0442b1
WilliamsFavourCheta <favourwills7@gmail.com>
2024-12-04 23:16:06 +0100

    Add files via upload

---
+++ class.js
@@ -0,0 +1,29 @@
+let student = {
+    name : "victoria",
+    age : 67,
+    hall : "nyberg",
+    matric : "24/2711",
+    SCHOOL :{
+        DEPT: "IT",
+        COURSE: "COSC 209"    
+    }
+};
+
+//console.log(student.name+ "        " + student.age);
+//console.log(student);
+
+// FUNCTIONS //
+function student(){
+let username = "amaka";
+var password = "1960";
+if (username == "amaka" && password =="1960"){
+    console.log("correct username !!!");
+    }else {
+    console.log("wrong user name !!!");
+    }
+}
+console.log (student());
+
+var chi= student();
+
+chi
